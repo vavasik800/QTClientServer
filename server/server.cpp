460eeef87d568bbbe2177a4908487acbec99ac3a
vavasik800 <74983123+vavasik800@users.noreply.github.com>
2023-08-14 23:11:05 +0300

    Создание server-приложения версии 4

--- server/server.cpp
+++ server/server.cpp
@@ -2,95 +2,115 @@
 #include "sqlliteDB.h"
 
 
-Server::Server() {
-    if(this->listen(QHostAddress::Any, 2323))
-        qDebug() << "start";
-    else
-        qDebug() << "error";
-}
+Server::Server() { }
+
 
 Server::Server(QString dirForFiles, QString pathFileDb) {
     this->dirForFiles = dirForFiles;
     db.setPath(pathFileDb);
+    if(this->listen(QHostAddress::Any, 2323))
+        qDebug() << "Server run";
+    else
+        qDebug() << "Server error";
 }
 
+// Основной метод, осуществляющий работу сервера.
+// Проверяет файлы в каталоге на изменения каждые 20 секунд.
 void Server::runServer()
 {
-    cout << "Server run" << endl;
-    cout << qPrintable(dirForFiles) << endl;
-    //    onTimeout();
     QTimer* timer = new QTimer(this);
     connect(timer, SIGNAL(timeout()), this, SLOT(onTimeout()));
-    timer->start(10000);
+    timer->start(20000);
 }
 
 
-void Server::SendToClient(QString str){
+void Server::SendToClient(QVector<QVector<QString>> vector){
     Data.clear();
     QDataStream out(&Data, QIODevice::WriteOnly);
     out.setVersion(QDataStream::Qt_5_14);
-    out << str;
+    out << vector;
     socket->write(Data);
 }
 
 QFileInfoList Server::getFiles()
 {
-    QDir dir;  //объявляем объект работы с папками
+    QDir dir; //объявляем объект работы с папками
     dir.setFilter(QDir::Files);
     dir.cd(dirForFiles);
-    QFileInfoList result = dir.entryInfoList();     //получаем список файлов директории
+    QFileInfoList result = dir.entryInfoList(); //получаем список файлов директории
     return result;
 }
 
+// Метод для работы с файлами .xml.
+// Здесь файлы парсятся, а затем данные сохраняются в БД.
+//  :param files: набор файлов для обработки
 void Server::workWithFiles(QFileInfoList files)
 {
     for (int i = 0; i < files.size(); ++i) {
         QFileInfo fileInfo = files.at(i);
-        QString nameFile = fileInfo.fileName();
+        QString nameFile = fileInfo.fileName(); // название файла
         cout << qPrintable(nameFile) << endl;
         QString patfFile = QString("%1/%2").arg(fileInfo.path()).arg(nameFile);
-        QString hashFile = getHashFile(patfFile);
+        QString hashFile = getHashFile(patfFile); // получение Хэша файла
 
         QString query = SELECT_FILE;
         query = query.arg(nameFile);
-
-        QVector<QVector<QString>> result = db.runQuerySelect(query);
+        // запрос данных о наличии данных файлов В БД
+        QVector<QVector<QString>> result = db.runQuery(query, "select");
         if (result.size() > 1) {
             cout << "Check DataBase. Double files" << endl;
             return;
         }
         if (result.size() == 0){
-            // парсинг файла
-            auto dataFromFile = parsingXml(patfFile);
-            auto newDataWithId = createGoodData(dataFromFile);
+            // Случай, когда файла нет в БД.
             query = QString(INSERT_FILE).arg(nameFile).arg(fileInfo.path()).arg(hashFile);
-            if(!db.runQueryInsert(query))
-                cout << "Bad Insert!" << endl;
-            for (auto elem: newDataWithId) {
-                if(!db.runQueryInsert(elem.keys()[0], elem.value(elem.keys()[0])))
-                    cout << "Bad Insert!" << endl;
+            db.runQuery(query, "insert"); // добавление файла и его хэша
+            // обработка файла
+            if (!workWithOneFile(patfFile)) {
+                cout << "File '%1' is bad! Check file, please!" << endl;
+                continue;
             }
         }
         else if (result[0][2] == hashFile) {
+            // Случай, когда файл не изменялся (хэши совпадают)
             cout << "Files in BD, Don't shanges." << endl;
             continue;
         }
         else {
-            // парсинг файла
-            auto dataFromFile = parsingXml(patfFile);
-            auto newDataWithId = createGoodData(dataFromFile);
+            // Случай, когда файл изменился.
+            auto ip = nameFile.mid(0, nameFile.size() - 4);
+            query = QString(DELETE_CASKADE_FILE).arg(ip);
+            db.runQuery(query, "delete"); // каскадное удаление данных связанных с измененнным файлом
+            // обработка файла
+            if (!workWithOneFile(patfFile)) {
+                cout << "File '%1' is bad! Check file, please!" << endl;
+                continue;
+            }
             // обновление хэша
             query = QString(UPDATE_HASH_FILE).arg(hashFile).arg(nameFile);
-            if(!db.runQueryUpdate(query))
-                cout << "Bad Update!" << endl;
-            for (auto elem: newDataWithId) {
-                if(!db.runQueryInsert(elem.keys()[0], elem.value(elem.keys()[0])))
-                    cout << "Bad Insert!" << endl;
-            }
+            db.runQuery(query, "update");
         }
     }
 }
 
+// Метод для обработки одного файла.
+bool Server::workWithOneFile(QString patfFile)
+{
+    auto dataFromFile = parsingXml(patfFile); // парсинг файла
+    dataFromFile.pop_front(); // удаление корневого элемента <net>
+    if (dataFromFile.size() == 0){
+        cout << "File '%1' is bad! Check file, please!" << endl;
+        return false;
+    }
+    auto newDataWithId = createGoodData(dataFromFile); // добавление привязки дочерних элементов к родительским
+    for (auto elem: newDataWithId) {
+        // добавление данных файла
+        db.runQuery(db.createQuery(elem.keys()[0], elem.value(elem.keys()[0])), "insert");
+    }
+    return true;
+}
+
+// Метод парсинга .xml файла. Сохранение всех тэгов и его атрибутов.
 QVector<QMap<QString, QMap<QString, QString>>> Server::parsingXml(QString pathFile)
 {
     QVector<QMap<QString, QMap<QString, QString>>> result;
@@ -109,8 +129,6 @@ QVector<QMap<QString, QMap<QString, QString>>> Server::parsingXml(QString pathFi
             continue;
         if (token == QXmlStreamReader::StartElement)
         {
-            if (xml.name() == "net")
-                continue;
             QMap<QString, QString> elem = parsingXmlBlock(xml.attributes());
             element[xml.name().toString()] = elem;
         }
@@ -120,6 +138,7 @@ QVector<QMap<QString, QMap<QString, QString>>> Server::parsingXml(QString pathFi
     return result;
 }
 
+// Метод парсинга одного тэга из файла .xml
 QMap<QString, QString> Server::parsingXmlBlock(QXmlStreamAttributes attrib)
 {
     QMap<QString, QString> elem;
@@ -127,47 +146,53 @@ QMap<QString, QString> Server::parsingXmlBlock(QXmlStreamAttributes attrib)
         elem[attrib[i].name().toString()] = attrib[i].value().toString();
     }
     return elem;
-
 }
 
+// Метод для привязки дочерних компонентов xml к родительским для дальнейшего их заноса в базу.
 QVector<QMap<QString, QMap<QString, QString>>> Server::createGoodData(QVector<QMap<QString, QMap<QString, QString>>> data)
 {
     QVector<QMap<QString, QMap<QString, QString>>> result;
-    QVector<QString> tags;
-    QVector<QString> idsTags;
+    QVector<QString> tags; // список тэгов для поиска
+    QVector<QString> idsTags; // список id тэгов для привязки
     for (auto elem: data) {
         QMap<QString, QMap<QString, QString>> row = elem;
-        QString tag = elem.keys()[0];
-        QString id = elem.value(elem.keys()[0]).value("id");
+        QString tag = elem.keys()[0]; // тэг на текущем шаге
+        QString id = elem.value(elem.keys()[0]).value("id"); // id на текущем шаге
         if (tags.size() == 0){
+            // если это первый элемент, то кладем их тэг и id в поисковые структуры
             tags.push_back(tag);
             idsTags.push_back(id);
             result.push_back(row);
             continue;
         }
-        int indexTag = tags.indexOf(tag);
+        int indexTag = tags.indexOf(tag); // проверяем наличие данного тэга в поисковых структурах
         if (indexTag != -1) {
+            // если такой тэг уже есть, то удалем все тэги после аналогичного (включительно)
+            // (верхний тэг будет родителем текущего)
             tags = tags.mid(0, indexTag);
             idsTags = idsTags.mid(0, indexTag);
         }
-        QString tagUp = tags.last();
+        QString tagUp = tags.last(); // последний тэг в поисковой структуре (родительский)
         QString idUp = idsTags.last();
+        // добавление нового поля id_*название родительского тэга* (для привязки к родителю)
         auto newMap = elem.value(elem.keys()[0]);
         newMap[QString("id_")+tagUp] = idUp;
         QMap<QString, QMap<QString, QString>> newRow;
         newRow[tag] = newMap;
         tags.push_back(tag);
         idsTags.push_back(id);
-        result.push_back(newRow);
+        result.push_back(newRow); // добавление в результат
     }
     return result;
 }
 
+// Метод, получения хэша файла
 QString Server::getHashFile(QString pathFile)
 {
     QString hashStr = "";
     QFile f(pathFile);
     if (f.open(QFile::ReadOnly)) {
+        // Применение хэш-алгоритма Sha1
         QCryptographicHash hash(QCryptographicHash::Sha1);
         if (hash.addData(&f)) {
             hashStr += QString(hash.result().toHex());
@@ -177,6 +202,7 @@ QString Server::getHashFile(QString pathFile)
 }
 
 void Server::incomingConnection(qintptr socketDescriptor){
+    qDebug() << "client77777777777" << socketDescriptor;
     socket = new QTcpSocket;
     socket->setSocketDescriptor(socketDescriptor);
     connect(socket, &QTcpSocket::readyRead, this, &Server::slotReadyRead);
@@ -193,7 +219,8 @@ void Server::slotReadyRead(){
         qDebug() << "read...";
         QString str;
         in >> str;
-        qDebug() << str;
+        auto result = db.runQuery(SELECT_BLOCK, "select");
+        SendToClient(result);
     }
     else
         qDebug() << "DataStream error";
@@ -201,6 +228,11 @@ void Server::slotReadyRead(){
 
 void Server::onTimeout()
 {
+    // проверка работы БД
+    if (!db.testRunBd()){
+         cout << "Don't work DB!" << endl;
+         return;
+    }
     cout << "Start read files" << endl;
     QFileInfoList files = this->getFiles();
     cout << "Finish read files" << endl;
